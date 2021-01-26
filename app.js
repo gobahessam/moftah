a8ab77850e4fe72d902d5ef27d339cba6debb364
gobahessam <65198407+gobahessam@users.noreply.github.com>
2021-01-26 21:02:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Not interested to writed I Am Rochie everywhere , just enjoy the code 😃
+
+alert ("مبروووك الف مبروووك يا مفتاح اعقبال ميه وعشرين سنه " )
+
+window.addEventListener("mouseover",play);
+window.addEventListener("auto",play);
+function play(){ document.getElementById("audio").play();
+}
